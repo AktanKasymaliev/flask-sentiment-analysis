38690a1e8df6a883b899ee20b3e8886e4f708636
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2023-05-13 22:44:00 +0600

    added vader model of sentiment analysis

---
+++ webapp/utils.py
@@ -0,0 +1,27 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def get_score_of_sentiment(text: str) -> float:
+    """
+    Returns a float score of sentiment of the text.
+
+    :param text: text to analyze
+    :return: float score of sentiment
+    """
+    sia = SentimentIntensityAnalyzer()
+    return sia.polarity_scores(text)['compound']
+
+def get_sentiment(score: float) -> int:
+    """
+    Returns an integer sentiment of the text.
+
+    :param score: float score of sentiment
+    :return: integer sentiment
+    """
+    if score >= 0.05:
+        return 1
+    elif score <= -0.05:
+        return -1
+    else:
+        return 0
+
+# Path: webapp\views.py
--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,20 @@
 from flask import render_template
+from flask import request
 
 from run import app
 from webapp.urls import URLPATTERNS
+from webapp.utils import get_score_of_sentiment, get_sentiment
 
-@app.route(URLPATTERNS["MAIN_PAGE"])
+@app.route(URLPATTERNS["MAIN_PAGE"], methods=["GET"])
 def main_page():
     return render_template("pages/home.html")
 
-@app.route(URLPATTERNS["RESULTS_PAGE"])
+@app.route(URLPATTERNS["RESULTS_PAGE"], methods=["POST"])
 def results_page():
-    return render_template("pages/results.html")
+    text = request.form.get("text")
+    if text:
+        score = get_score_of_sentiment(text)
+        sentiment = get_sentiment(score)
+    else: 
+        sentiment = None
+    return render_template("pages/results.html", sentiment=sentiment)
